3b2ad70c0f9a79013f696471557d5c5fab2bff69
Huey <jasperjeng@gmail.com>
2017-09-02 17:29:56 -0700

    convert files to linux file ending

--- install.sh
+++ install.sh
@@ -1,33 +1,44 @@
 git clone --quiet https://github.com/hueyjj/vim-airline
 echo "Vim airline... ok"
-mv vim-airline .vim
 
 git clone --quiet https://github.com/hueyjj/vim-airline-themes
 echo "Vim airline themes... ok"
-mv vim-airline-themes .vim
 
 git clone --quiet https://github.com/hueyjj/nerdtree
 echo "Nerdtree... ok"
-mv nerdtree .vim
 
-git clone --quiet https://github.com/hueyjj/supertab
+GIT Clone --quiet https://github.com/hueyjj/supertab
 echo "Supertab ...ok"
-mv supertab .vim
 
 echo ".vim moved to home directory"
-mv .vim ~/
 
 os=$(uname)
 case "$os" in
         "Linux")
         {
             echo "Operating system: Linux"
+            
+            {
+            find . -type f -print0 | xargs -0 dos2unix      # Convert dos (windows) files to unix/linux
+            } &> /dev/null
+            
+            echo "Moved .vimrc to home directory"
             mv _vimrc .vimrc
             mv .vimrc ~/
         };;
         *)
         {
-            echo "Don't know OS: rename vimrc respectively for OS \
-                  .vimrc, _vimrc, etc. Then, place in home directory."
+            echo "Moved _vimrc to home directory"
+            mv _vimrc ~/
         };;
 esac
+
+echo "Plugins moved to .vim"
+mv vim-airline .vim
+mv vim-airline-themes .vim
+mv nerdtree .vim
+mv supertab .vim
+
+echo ".vim moved to home directory"
+mv .vim ~/
+
